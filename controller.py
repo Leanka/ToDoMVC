681270b4e1b876fa2757365210cb5db56023331f
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-28 23:35:54 +0200

    Correct type of error, change input message

--- controller.py
+++ controller.py
@@ -9,13 +9,13 @@ def take_user_input(input_message=' ', max_input_length=1):
 def take_task_index(task_list_length=0):
     while True:
         try:
-            task_index = int(take_user_input('Pass tasks number: ', len(str(task_list_length))))
+            task_index = int(take_user_input('Pass tasks number', len(str(task_list_length))))
             task_index -= 1  # Decrement user input by one, prior checking its validation as list index
             if task_index not in range(task_list_length):
                 raise IndexError("Task position out of scope.\n")
         except IndexError as error_message:
             print(error_message)  # show message from view?
-        except TypeError as error_message:
+        except ValueError as error_message:
             print(error_message)  # show message from view?
         else:
             return task_index

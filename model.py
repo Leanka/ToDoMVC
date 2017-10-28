4eccac52ec267d07310a06c2c616d2a2e3466ce1
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-28 19:29:16 +0200

    Create frame of class

--- model.py
+++ model.py
@@ -1,7 +1,21 @@
 
 class Task():
-    pass
+    
+    def __init__(self):
+        pass
 
+    def mark_as_done(self):
+        pass
+
+    def mark_as_undone(self):
+        pass
+
+    def change_name(self):
+        pass
+
+    def change_description(self):
+        pass
+        
 class TaskList():
     pass
 

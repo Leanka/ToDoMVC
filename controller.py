0c02f88c621b436c02798e304209afebfcca8ae0
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-28 20:37:23 +0200

    Create frame for controller module

--- controller.py
+++ controller.py
@@ -1,5 +1,20 @@
+
+def take_user_input():
+    pass
+
+def take_task_index():
+    pass
+
+def prepare_task_to_be_viewed():
+    pass
+
+def prepare_task_list_to_be_viewed():
+    pass
+
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+
+    

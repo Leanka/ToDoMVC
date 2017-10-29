c0761c8c4e5c29224139271c999bfac7d97a8398
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-29 18:54:37 +0100

    Rewrite display_tasks_list to work on list

--- view.py
+++ view.py
@@ -1,12 +1,22 @@
-def display_tasks_list(tasks_dict):
-    title_bar = "|{:^3}|{:^5}|{:^20}|".format('', 'id', 'name')
+def display_tasks_list(tasks_attributes_lists):
+    first_column_title = ''
+    second_column_title = 'id'
+    third_column_title = 'name'
+
+    id_list_index = 0
+    name_list_index = 1
+
+    title_bar = "|{:^3}|{:^10}|{:^20}|".format(first_column_title, second_column_title, third_column_title)
     separator = '-'*len(title_bar)
 
     print(separator)
     print(title_bar)
     print(separator)
-    for index in range(len(tasks_dict['name'])):
-        print("|{:^3}|{:>5}|{:>20}|".format(index+1, tasks_dict['id'][index], tasks_dict['name'][index]))
+
+    for index in range(len(tasks_attributes_lists[id_list_index])):
+        print("|{:^3}|{:^10}|{:>20}|".format(index+1, 
+                                            tasks_attributes_lists[id_list_index][index], 
+                                            tasks_attributes_lists[name_list_index][index]))
         print(separator)
 
     # ValueError: Invalid format specifier if trying to use vars inside format brackets
@@ -33,5 +43,4 @@ def display_main_menu():
 
     for operation in menu:
         print(operation)
-
 

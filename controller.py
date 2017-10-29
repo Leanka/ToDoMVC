557cdcd4482442ea7b111b166b52ff041ba68dac
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-29 02:05:41 +0100

    Change parameter from default to not default one

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ def take_user_input(input_message=' ', max_input_length=1):
         if (len(users_input) != 0) and (len(users_input) <= max_input_length):
             return users_input
 
-def take_task_index(task_list_length=0):
+def take_task_index(task_list_length):
     while True:
         try:
             task_index = int(take_user_input('Pass tasks number', len(str(task_list_length))))
@@ -44,17 +44,19 @@ def main():
             pass
         elif chosen_menu_option == "2":  # Display specific item's details
             pass
+
         elif chosen_menu_option == "3":  # Add item
             task_name = take_user_input('Pass tasks name (max 20 char)', 20)
             task_description = take_user_input('Pass tasks description (max 150 char)', 150)
             tasks_to_do.add_task(task_name, task_description)
+            # print item has beed add/ed?/ to the list! message
 
         elif chosen_menu_option == "4":  # Change items name
             chosen_task_index = take_task_index(len(tasks_to_do.todo_list))
             tasks_new_name = take_user_input('Pass tasks name (max 20 char)', 20)
             tasks_to_do.todo_list[chosen_task_index].change_name(tasks_new_name)
             # print description has been changed message
-            
+
         elif chosen_menu_option == "5":  # Change items desctiption
             chosen_task_index = take_task_index(len(tasks_to_do.todo_list))
             tasks_new_description = take_user_input('Pass tasks description (max 150 char)', 150)

b05b03417509aac1c0d09af09f17158aef18e8cc
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-29 17:01:39 +0100

    Add parameter instructions to display_operation_communicate

--- view.py
+++ view.py
@@ -6,8 +6,8 @@ def display_single_task_info():
     pass
 
 
-def display_operation_communicate(noun='something', past_participle_varb='done'):
-    print("{} has been {} successfully!".format(noun.title(), past_participle_varb.lower()))
+def display_operation_communicate(noun='something', past_participle_varb='done', instructions=''):
+    print("{} has been {}! {}".format(noun.title(), past_participle_varb.lower(), instructions))
 
 def display_main_menu():
     menu = [" 1. Display items list",

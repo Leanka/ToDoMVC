372d5c4324242db75d430bc7d5deb4bcea8aca4b
Leanka <comamydzisiajnasniadanie@gmail.com>
2017-10-29 16:39:59 +0100

    Create and fill display_main_menu

--- view.py
+++ view.py
@@ -6,6 +6,19 @@ def display_single_task_info():
     pass
 
 
-def display_operation_communicate(noun='Something', past_participle_varb='done'):
-    print("{} has been {} successfully".format(noun.title(), past_participle_varb.lower()))
+def display_operation_communicate(noun='something', past_participle_varb='done'):
+    print("{} has been {} successfully!".format(noun.title(), past_participle_varb.lower()))
+
+def display_main_menu():
+    menu = [" 1. Display items list",
+            " 2. Display specific item's details",
+            " 3. Add item",
+            " 4. Change items name",
+            " 5. Change items desctiption",
+            " 6. Mark item as done",
+            " 7. Delete item",
+            " 8. Exit"]
+
+    for operation in menu:
+        print(operation)
 
